f3efac19ee2269b1bc7ef0c149179af0626b8a32
joaovic499 <joaovictorsilva60@live.com>
2024-04-23 15:54:04 -0300

    API JSON FINAL

--- index.js
+++ index.js
@@ -7,94 +7,126 @@ const PORT = 3000;
 app.use(express.json());
 
 const usuariosFilePath = 'usuarios.json';
+let proximoId = 1;
 
-
-app.get('/usuarios', (req, res) =>{
-    fs.readFile(usuariosFilePath, 'utf8', (err,data) => {
-        if (err) {
-            console.error(err)
-            return res.status(500).send('Erro Interno do Servidor');
-        }
+// Rota para listar todos os usuários
+app.get('/usuarios', (req, res) => {
+  fs.readFile(usuariosFilePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Erro interno do servidor');
+    }
 
     const usuarios = JSON.parse(data);
     res.json(usuarios);
-    });
+  });
 });
 
-app.post('/usuarios', (req, res ) => {
-    const { usuario, senha, codigo } = req.body;
-    if(!usuario || !senha || codigo ) {
-        return res.status(400).send("Usuario, senha e codigo sao obrigatorios");
+// Rota para buscar um usuário pelo ID
+app.get('/usuarios/:id', (req, res) => {
+  const id = req.params.id;
+
+  fs.readFile(usuariosFilePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Erro interno do servidor');
     }
 
-    fs.readFile(usuariosFilePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error(err)
-            return res.status(500).send('Erro interno do servidor');
-        }
-    
-        res.status(201).send("Usuario Criado com sucesso");
-    });
+    const usuarios = JSON.parse(data);
+    const usuario = usuarios.find(u => u.id === parseInt(id));
+
+    if (!usuario) {
+      return res.status(404).send('Usuário não encontrado');
+    }
+
+    res.json(usuario);
+  });
 });
 
+// Rota para criar um novo usuário
+app.post('/usuarios', (req, res) => {
+  const { usuario, senha } = req.body;
+  if (!usuario || !senha) {
+    return res.status(400).send('Usuário e senha são obrigatórios');
+  }
 
-app.put('/usuarios/:id', (req, res) =>{
-    const id = req.params.id;
-    const { usuario, senha, codigo } = req.body;
+  const novoUsuario = { id: proximoId++, usuario, senha };
+  fs.readFile(usuariosFilePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Erro interno do servidor');
+    }
 
-    fs.readFile(usuariosFilePath, 'utf8', (err,data) =>{
-        if (err) {
-            console.error(err);
-            return res.status(500).send('Erro interno do servidor');
-        }
+    const usuarios = JSON.parse(data);
+    usuarios.push(novoUsuario);
 
-        const usarios = JSON.parse(data);
-        const usuarioIndex = usuarios.findIndex(u => u.codigo === id);
+    fs.writeFile(usuariosFilePath, JSON.stringify(usuarios), err => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Erro interno do servidor');
+      }
 
-        if (usuarioIndex === -1){
-            return res.status(404).send('Usuario nao encontrado');
-        }
-
-        usuarios[usuarioIndex] = { usuario, senha, codigo: id};
-
-        fs.writeFile(usuariosFilePath, JSON.stringify(usuarios), err =>{
-            if (err) {
-                console.error(err);
-                return res.status(500).send('Erro interno do servidor');
-            }
-        
-        res.send('Usuario atualizado com sucesso');
-        });
+      res.status(201).send('Usuário criado com sucesso');
     });
- });
+  });
+});
 
+// Rota para atualizar um usuário existente
+app.put('/usuarios/:id', (req, res) => {
+  const id = req.params.id;
+  const { usuario, senha } = req.body;
 
- //Rota para deletar um usuario
+  fs.readFile(usuariosFilePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Erro interno do servidor');
+    }
 
- app.delete('/usuarios/:id', (req, res) =>{
-    const id = req.params.id;
+    const usuarios = JSON.parse(data);
+    const usuarioIndex = usuarios.findIndex(u => u.id === parseInt(id));
 
-    fs.readFile(usuariosFilePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).send('Erro interno do servidor');
-        }
+    if (usuarioIndex === -1) {
+      return res.status(404).send('Usuário não encontrado');
+    }
 
-        let usuarios = JSON.parse(data);
-        usuarios = usuarios.filter(u => u.codigo !== id);
+    usuarios[usuarioIndex] = { id: parseInt(id), usuario, senha };
 
-        fs.writeFile(usuariosFilePath, JSON.stringify(usuarios), err => {
-            if (err){
-                console.error(err);
-                return res.status(500).send('Erro interno do servidor');
-            }
-        
-            res.send('Usuario deletado com sucesso');
+    fs.writeFile(usuariosFilePath, JSON.stringify(usuarios), err => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Erro interno do servidor');
+      }
 
-         });
+      res.send('Usuário atualizado com sucesso');
     });
- });
+  });
+});
 
- app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
- });
+// Rota para deletar um usuário
+app.delete('/usuarios/:id', (req, res) => {
+  const id = req.params.id;
+
+  fs.readFile(usuariosFilePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Erro interno do servidor');
+    }
+
+    let usuarios = JSON.parse(data);
+    usuarios = usuarios.filter(u => u.id !== parseInt(id));
+
+    fs.writeFile(usuariosFilePath, JSON.stringify(usuarios), err => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Erro interno do servidor');
+      }
+
+      res.send('Usuário deletado com sucesso');
+    });
+  });
+});
+
+// Inicia o servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
